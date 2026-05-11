8cac811bb06842e972aa33679ae82427965fea5d
xshem-doctor <sedrashahen11@gmail.com>
2026-05-11 09:15:47 -0400

    move to vercel

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Delta Wallet",
   url: "https://deltawallet.app",
-  apkUrl: "https://github.com/xshem-doctor/deltawalletweb/releases/download/v1.0.0/delta-wallet.apk",
+  apkUrl: "https://j8qfljajkvrcqfbe.public.blob.vercel-storage.com/delta-walletv1-0-0.apk",
   supportEmail: "support@deltawallet.app",
-
+  
 };
