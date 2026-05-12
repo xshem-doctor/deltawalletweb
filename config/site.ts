25c85a227f5e0b10a13279c49d8f36bd4c3ec4c3
xshem-doctor <sedrashahen11@gmail.com>
2026-05-12 10:22:22 -0400

    delta wallet release update 1.0.1

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Delta Wallet",
   url: "https://deltawallet.app",
-  apkUrl: "https://j8qfljajkvrcqfbe.public.blob.vercel-storage.com/delta-walletv1-0-0.apk",
+  apkUrl: "https://j8qfljajkvrcqfbe.public.blob.vercel-storage.com/delta-walletv1-0-1.apk",
   supportEmail: "support@deltawallet.app",
   
 };
